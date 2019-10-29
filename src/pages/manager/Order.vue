61ec4abbcda82df45b73c23ef7db49c527fc1031
= <=>
2019-10-29 15:22:41 +0800

    weisy修改订单标签页

--- src/pages/manager/Order.vue
+++ src/pages/manager/Order.vue
@@ -6,9 +6,10 @@
         />
         <!-- 标签页 -->
         <van-tabs v-model="active">
-            <van-tab title="全部订单">全部订单</van-tab>
-            <van-tab title="待接单">待结单</van-tab>
-            <van-tab title="待完成">待完成</van-tab>
+            <van-tab title="所有订单">所有订单</van-tab>
+            <van-tab title="未接订单">未接订单</van-tab>
+            <van-tab title="已接订单">已接订单</van-tab>
+            <van-tab title="待确认">待确认</van-tab>
             <van-tab title="已完成">已完成</van-tab>
         </van-tabs>
     </div>
