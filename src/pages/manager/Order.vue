dc2474e302e1ec62717779242fd2a3ca3c488b85
= <=>
2019-10-30 11:28:27 +0800

    weisy完成订单页面相应跳转

--- src/pages/manager/Order.vue
+++ src/pages/manager/Order.vue
@@ -9,10 +9,13 @@
             <!-- 所有订单 -->
             <van-tab title="所有订单">
                 <!-- 根据状态判断获取不同的面板订单信息 -->
-                <!-- 待确认、已完成 -->
-                <!-- <van-panel v-if="status == '' " title="顾客1" status="状态">
-                    <div>相关信息</div>
-                </van-panel> -->
+                <div v-for="o in orders" :key="o.id">
+                    <!-- 待确认、已完成 -->
+                    <div v-if="o.status === '待确认' || o.status === '已完成' ">
+                        <van-panel :title="o.customer.realname" :status="o.status" :key="o.id">
+                            <div>相关信息</div>
+                        </van-panel>
+                    </div>
                 <!-- 未接订单 -->
                 <!-- <van-panel v-else-if="status == '' " title="顾客1" status="状态">
                     <div>相关信息</div>
@@ -37,6 +40,7 @@
                         </van-row>
                     </div>
                 </van-panel> -->
+               </div>
 
                 <!-- 将查询信息传给面板展示订单信息 -->
                 <van-panel title="顾客1" status="状态">
@@ -47,58 +51,90 @@
             <van-tab title="未接订单">
                 <!-- 遍历查询信息 -->
                 <!-- 将查询信息展示在面板 -->
-                <van-panel title="顾客1" status="状态">
-                    <div>相关信息</div>
-                    <div slot="footer">
-                        <van-row>
-                            <van-col offset="16" span="16">
-                                <van-button size="small" plain type="danger" @click="refusedHandler">拒绝</van-button>
-                                &nbsp;&nbsp;&nbsp;
-                                <van-button size="small" plain type="primary" @click="acceptHandler">接受</van-button>
-                            </van-col>
-                        </van-row>
+                <div v-for="o in orders" :key="o.id" >
+                    <div v-if="o.status === '待接单'">
+                        <van-panel :title="o.customer.realname" :status="o.status" :key="o.id" >
+                            <div>相关信息</div>
+                            <div slot="footer">
+                                <van-row>
+                                    <van-col offset="16" span="16">
+                                        <van-button size="small" plain type="danger" @click="refusedHandler">拒绝</van-button>
+                                        &nbsp;&nbsp;&nbsp;
+                                        <van-button size="small" plain type="primary" @click="acceptHandler">接受</van-button>
+                                    </van-col>
+                                </van-row>
+                            </div>
+                        </van-panel>
                     </div>
-                </van-panel>
+                </div>
             </van-tab>
             <!-- 已接订单 -->
             <van-tab title="已接订单">
-                <van-panel title="顾客1" status="状态">
-                    <div>相关信息</div>
-                    <div slot="footer">
-                        <van-row>
-                            <van-col offset="20" span="20">
-                                <van-button size="small" plain type="danger" @click="completeHandler">完成</van-button>
-                            </van-col>
-                        </van-row>
+                <div v-for="o in orders" :key="o.id">
+                    <div v-if="o.status === '待完成'">
+                        <van-panel :title="o.customer.realname" :status="o.status" :key="o.id">
+                            <div>相关信息</div>
+                            <div slot="footer">
+                                <van-row>
+                                    <van-col offset="20" span="20">
+                                        <van-button size="small" plain type="danger" @click="completeHandler">完成</van-button>
+                                    </van-col>
+                                </van-row>
+                            </div>
+                        </van-panel>
                     </div>
-                </van-panel>
+                </div>
             </van-tab>
             <!-- 待确认 -->
             <van-tab title="待确认">
-                <van-panel title="顾客1" status="状态">
-                    <div>
-                        相关信息
+                <div v-for="o in orders" :key="o.id">
+                    <div v-if="o.status === '待确认'">
+                        <van-panel :title="o.customer.realname" :status="o.status">
+                            <!-- {{o.customer.realname}} -->
+                            <div>
+                                相关信息
+                            </div>
+                        </van-panel>
                     </div>
-                </van-panel>
+                </div>
             </van-tab>
             <!-- 已完成 -->
             <van-tab title="已完成">
-                <van-panel title="顾客1" status="状态">
-                    <div>相关信息</div>
-                </van-panel>
+                <div v-for="o in orders" :key="o.id">
+                    <div v-if="o.status === '已完成'">
+                        <van-panel :title="o.customer.realname" :status="o.status">
+                            <div>相关信息</div>
+                        </van-panel>
+                    </div>
+                </div>
             </van-tab>
         </van-tabs>
     </div>
 </template>
 
 <script>
+import {mapState,mapGetters,mapMutations,mapActions} from 'vuex'
 export default {
     data(){
         return{
-            active:0
+            active:0,
         }
     },
+    created(){
+        this.loadData();
+    },
+    computed:{
+        ...mapState("order",["orders"]),
+        ...mapState("user",["infoUser"]),
+        ...mapGetters("order",["orderStatusFilter"])
+    },
     methods:{
+        ...mapActions("order",["findAllOrders"]),
+
+        loadData(){
+            this.findAllOrders(this.infoUser.id)
+        },
+
         refusedHandler(){
             alert("拒绝");
         },
@@ -107,7 +143,7 @@ export default {
         },
         completeHandler(){
             alert("完成")
-        }
+        },
     }
 }
 </script>
--- src/store/order.js
+++ src/store/order.js
@@ -1,16 +1,43 @@
 import {get,post,post_array} from '../http/axios'
 export default{
     namespaced:true,
-    states:{
-
+    state:{
+        orders:[],
+        // infoUser:{}
     },
     getters:{
-
+        countOrders(state){
+            // 统计订单个数
+            return state.orders.length
+        },
+        // 需要为获取器传递参数的写法
+        orderStatusFilter(state){
+            // 对订单的状态进行过滤
+            return function(status){
+                // 如果传递的状态存在则返回过滤
+                if(status){
+                    return state.orders.filter(item => item.status === status)
+                }else{
+                    // 不存在则返回全部
+                    return state.orders
+                }
+            }
+        }
     },
     mutations:{
-
+        // 刷新订单信息
+        refreshOrder(state,orders){
+            state.orders = orders
+        }
     },
     actions:{
+        // 查询所有订单信息
+        async findAllOrders(context,id){
+            // 获取订单信息
+            const response = await get('/order/query',{waiterId:id});
+            // 将订单信息设置到state.order中
+            context.commit('refreshOrder',response.data);
+        }
         
     }
 }
